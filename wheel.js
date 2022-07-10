582caccd99993da0bb69754ce8143c4a2a88eab3
Viktor <vktrgavryliuk@gmail.com>
2022-07-10 12:48:17 +0300

    scroll2

--- wheel.js
+++ wheel.js
@@ -8,7 +8,6 @@
     const emailWrapper = document.querySelector('.email-wrapper')
 
 
-
     let deg = 0;
     let spins = 2;
     let zoneSize = 36; // deg
@@ -67,13 +66,8 @@
     const showModal = () => {
         const modalForm = document.querySelector('.modal-wrapper');
         modalForm.classList.add('show')
-        // const socialForm = document.querySelector('.social-wrapper');
-        // socialForm.classList.add('show')
 
         body.classList.add('noScroll')
-        inputWrapper.classList.add('scroll')
-        emailWrapper.classList.add('scroll')
-
     }
 
     const handleWin = (actualDeg) => {
